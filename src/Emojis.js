21d90392675484d32e97a31faf3434e2ba2bf835
Rebecca Lambert <rebeccalambert@berkeley.edu>
2019-12-28 11:29:38 -0800

    Adds highlight on selection

--- src/Emojis.js
+++ src/Emojis.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import "./Emojis.css";
 
-function Emojis( {update, guests, review} ) {
+function Emojis({ update, guests, review }) {
   
     let emojis_array = "😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍 😘 🥰 😗 😙 😚 🙂 🤗 🤩 🤔 🤨 😐 😑 😶 🙄 😏 😣 😥 😮 🤐 😯 😪 😫 😴 😌 😛 😜 😝 🤤 😒 😓 😔 😕 🙃 🤑 😲 🙁 😖 😞 😟 😤 😢 😭 😦 😧 😨 😩 🤯 😬 😰 😱 🥵 🥶 😳 🤪 😵 😡 😠 🤒 🤢 🤮 🤧 😇 🥳 🥴 🥺 🧐 🤓 😈 💀 👻 💩 🙌 💃 🕺 🏆 🥇 🥈 🥉 🌠 🎊 🎉 🧡 💚 💙 💜 🖤 💔".split(" ")
 
-
-    let emojis = emojis_array.map( ( emoji, idx) => {
+    let emojis = emojis_array.map( (emoji, idx) => {
         return <li className="emoji" key={`emojis-${idx}`}>{emoji}</li>
     });
 
-    const newEmoji = (emoji) => {
-       update(guests, review, emoji)
+    const removeHighlight = () => {
+        let els = document.getElementsByClassName('emoji')
+        for (let i = 0; i < els.length; i++) {
+            els[i].style.cssText = 'text-shadow: none;'
+        }
     }
-    
+
+    const newEmoji = (e) => {        
+        removeHighlight();
+        (e.target).style.cssText = 'text-shadow: 0 0 0.5em gold;'
+        update(guests, review, e.target.innerText)
+    }
+
     return (
-        
-        <ul className="emoji-list" onClick={e=> newEmoji(e.target.innerText)}>{emojis}</ul>
-       
+        <ul className="emoji-list" onClick={e => newEmoji(e)}>{emojis}</ul>
     );
 }
 
